359196c81bd7105c9a4a89b9d1c4ebe40c3c0186
Sridhar Ramachandran <lambdazen@gmail.com>
2018-07-15 18:45:41 -0400

    Fix for issue #11

--- src/main/java/com/lambdazen/bitsy/gremlin/BitsyGraphStep.java
+++ src/main/java/com/lambdazen/bitsy/gremlin/BitsyGraphStep.java
@@ -35,105 +35,95 @@ public final class BitsyGraphStep<S, E extends Element> extends GraphStep<S, E> 
         this.setIteratorSupplier(() -> (Iterator<E>) (Vertex.class.isAssignableFrom(this.returnClass) ? this.vertices() : this.edges()));
     }
 
-	private BitsyGraph getBitsyGraph() {
-		Graph baseGraph = this.getTraversal().getGraph().get();
+    private BitsyGraph getBitsyGraph() {
+        Graph baseGraph = this.getTraversal().getGraph().get();
         final BitsyGraph graph;
-		if (baseGraph instanceof BitsyAutoReloadingGraph) {
-			return ((BitsyAutoReloadingGraph)baseGraph).getBaseGraph();
-		} else {
-			return (BitsyGraph) baseGraph;
-		}
-	}
+        if (baseGraph instanceof BitsyAutoReloadingGraph) {
+            return ((BitsyAutoReloadingGraph)baseGraph).getBaseGraph();
+        } else {
+            return (BitsyGraph) baseGraph;
+        }
+    }
 
     private Iterator<? extends Vertex> vertices() {
         return lookupVertices(getBitsyGraph(), this.hasContainers, this.ids);
     }
 
 
-	private Iterator<Vertex> lookupVertices(final BitsyGraph graph, final List<HasContainer> hasContainers, final Object... ids) {
-		// ids are present, filter on them first
-		if (ids.length > 0)
-			return IteratorUtils.filter(graph.vertices(ids), vertex -> HasContainer.testAll(vertex, hasContainers));
+    private Iterator<Vertex> lookupVertices(final BitsyGraph graph, final List<HasContainer> hasContainers, final Object... ids) {
+        // ids are present, filter on them first
+        if (ids.length > 0)
+            return IteratorUtils.filter(graph.vertices(ids), vertex -> HasContainer.testAll(vertex, hasContainers));
 
-		// get a label being search on
-		Optional<String> label = hasContainers.stream()
-				.filter(hasContainer -> hasContainer.getKey().equals(T.label.getAccessor()))
-				.filter(hasContainer -> Compare.eq == hasContainer.getBiPredicate())
-				.map(hasContainer -> (String) hasContainer.getValue())
-				.findAny();
-		if (!label.isPresent())
-			label = hasContainers.stream()
-			.filter(hasContainer -> hasContainer.getKey().equals(T.label.getAccessor()))
-			.map(hasContainer -> (String) hasContainer.getValue())
-			.findAny();
+        // get a label being search on
+        Optional<String> label = hasContainers.stream()
+            .filter(hasContainer -> hasContainer.getKey().equals(T.label.getAccessor()))
+            .filter(hasContainer -> Compare.eq == hasContainer.getBiPredicate())
+            .map(hasContainer -> (String) hasContainer.getValue())
+            .findAny();
 
-		// Labels aren't indexed in Bitsy, only keys -- so do a full scan
-		for (final HasContainer hasContainer : hasContainers) {
-			if (Compare.eq == hasContainer.getBiPredicate() && !hasContainer.getKey().equals(T.label.getAccessor())) {
-				if (graph.getIndexedKeys(Vertex.class).contains(hasContainer.getKey())) {
-					// Find a vertex by key/value 
-					return IteratorUtils.stream(graph.verticesByIndex(hasContainer.getKey(), hasContainer.getValue()))
-							.map(vertex -> (Vertex) vertex)
-							.filter(vertex -> HasContainer.testAll(vertex, hasContainers)).iterator();
-				}
-			}
-		}
-		return IteratorUtils.filter(graph.vertices(), vertex -> HasContainer.testAll(vertex, hasContainers));
-	}
+        // Labels aren't indexed in Bitsy, only keys -- so do a full scan
+        for (final HasContainer hasContainer : hasContainers) {
+            if (Compare.eq == hasContainer.getBiPredicate() && !hasContainer.getKey().equals(T.label.getAccessor())) {
+                if (graph.getIndexedKeys(Vertex.class).contains(hasContainer.getKey())) {
+                    // Find a vertex by key/value
+                    return IteratorUtils.stream(graph.verticesByIndex(hasContainer.getKey(), hasContainer.getValue()))
+                        .map(vertex -> (Vertex) vertex)
+                        .filter(vertex -> HasContainer.testAll(vertex, hasContainers)).iterator();
+                }
+            }
+        }
+        return IteratorUtils.filter(graph.vertices(), vertex -> HasContainer.testAll(vertex, hasContainers));
+    }
 
     private Iterator<? extends Edge> edges() {
         //return IteratorUtils.filter(this.getTraversal().getGraph().get().edges(this.ids), edge -> HasContainer.testAll(edge, this.hasContainers));
         return lookupEdges(getBitsyGraph(), this.hasContainers, this.ids);
     }
 
-	private Iterator<Edge> lookupEdges(final BitsyGraph graph, final List<HasContainer> hasContainers, final Object... ids) {
-		// ids are present, filter on them first
-		if (ids.length > 0)
-			return IteratorUtils.filter(graph.edges(ids), vertex -> HasContainer.testAll(vertex, hasContainers));
+    private Iterator<Edge> lookupEdges(final BitsyGraph graph, final List<HasContainer> hasContainers, final Object... ids) {
+        // ids are present, filter on them first
+        if (ids.length > 0)
+            return IteratorUtils.filter(graph.edges(ids), vertex -> HasContainer.testAll(vertex, hasContainers));
 
-		// get a label being search on
-		Optional<String> label = hasContainers.stream()
-				.filter(hasContainer -> hasContainer.getKey().equals(T.label.getAccessor()))
-				.filter(hasContainer -> Compare.eq == hasContainer.getBiPredicate())
-				.map(hasContainer -> (String) hasContainer.getValue())
-				.findAny();
-		if (!label.isPresent())
-			label = hasContainers.stream()
-			.filter(hasContainer -> hasContainer.getKey().equals(T.label.getAccessor()))
-			.map(hasContainer -> (String) hasContainer.getValue())
-			.findAny();
+        // get a label being search on
+        Optional<String> label = hasContainers.stream()
+            .filter(hasContainer -> hasContainer.getKey().equals(T.label.getAccessor()))
+            .filter(hasContainer -> Compare.eq == hasContainer.getBiPredicate())
+            .map(hasContainer -> (String) hasContainer.getValue())
+            .findAny();
 
-		// Labels aren't indexed in Bitsy, only keys -- so do a full scan
-		for (final HasContainer hasContainer : hasContainers) {
-			if (Compare.eq == hasContainer.getBiPredicate() && !hasContainer.getKey().equals(T.label.getAccessor())) {
-				if (graph.getIndexedKeys(Vertex.class).contains(hasContainer.getKey())) {
-					// Find a vertex by key/value 
-					return IteratorUtils.stream(graph.edgesByIndex(hasContainer.getKey(), hasContainer.getValue()))
-							.map(edge -> (Edge) edge)
-							.filter(edge -> HasContainer.testAll(edge, hasContainers)).iterator();
-				}
-			}
-		}
-		return IteratorUtils.filter(graph.edges(), vertex -> HasContainer.testAll(vertex, hasContainers));
-	}
+        // Labels aren't indexed in Bitsy, only keys -- so do a full scan
+        for (final HasContainer hasContainer : hasContainers) {
+            if (Compare.eq == hasContainer.getBiPredicate() && !hasContainer.getKey().equals(T.label.getAccessor())) {
+                if (graph.getIndexedKeys(Vertex.class).contains(hasContainer.getKey())) {
+                    // Find a vertex by key/value
+                    return IteratorUtils.stream(graph.edgesByIndex(hasContainer.getKey(), hasContainer.getValue()))
+                        .map(edge -> (Edge) edge)
+                        .filter(edge -> HasContainer.testAll(edge, hasContainers)).iterator();
+                }
+            }
+        }
+        return IteratorUtils.filter(graph.edges(), vertex -> HasContainer.testAll(vertex, hasContainers));
+    }
 
-	@Override
-    public String toString() {
+    @Override
+        public String toString() {
         if (this.hasContainers.isEmpty())
             return super.toString();
         else
             return 0 == this.ids.length ?
-                    StringFactory.stepString(this, this.returnClass.getSimpleName().toLowerCase(), this.hasContainers) :
-                    StringFactory.stepString(this, this.returnClass.getSimpleName().toLowerCase(), Arrays.toString(this.ids), this.hasContainers);
+                StringFactory.stepString(this, this.returnClass.getSimpleName().toLowerCase(), this.hasContainers) :
+                StringFactory.stepString(this, this.returnClass.getSimpleName().toLowerCase(), Arrays.toString(this.ids), this.hasContainers);
     }
 
     @Override
-    public List<HasContainer> getHasContainers() {
+        public List<HasContainer> getHasContainers() {
         return Collections.unmodifiableList(this.hasContainers);
     }
 
     @Override
-    public void addHasContainer(final HasContainer hasContainer) {
+        public void addHasContainer(final HasContainer hasContainer) {
         if (hasContainer.getPredicate() instanceof AndP) {
             for (final P<?> predicate : ((AndP<?>) hasContainer.getPredicate()).getPredicates()) {
                 this.addHasContainer(new HasContainer(hasContainer.getKey(), predicate));
@@ -143,7 +133,7 @@ public final class BitsyGraphStep<S, E extends Element> extends GraphStep<S, E> 
     }
 
     @Override
-    public int hashCode() {
+        public int hashCode() {
         return super.hashCode() ^ this.hasContainers.hashCode();
     }
 }
---
+++ src/test/java/com/lambdazen/bitsy/structure/HasLabelTest.java
@@ -0,0 +1,36 @@
+import com.lambdazen.bitsy.BitsyGraph;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.T;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HasLabelTest {
+    @Test
+    public void bitsyPredicateTest() {
+        Graph g = new BitsyGraph();
+
+        final List<Object> props1 = new ArrayList<>();
+        props1.add(T.label);
+        props1.add("vert1");
+        props1.add("p1");
+        props1.add(1);
+        props1.add("p2");
+        props1.add("2");
+        g.addVertex(props1.toArray());
+
+        final List<Object> props2 = new ArrayList<>();
+        props2.add(T.label);
+        props2.add("vert2");
+        props2.add("2p1");
+        props2.add(1);
+        props2.add("2p2");
+        props2.add("2");
+        Object vert2 = g.addVertex(props2.toArray());
+
+        assertTrue(g.traversal().V().hasLabel(P.within("vert2")).next().equals(vert2));
+    }
+}
